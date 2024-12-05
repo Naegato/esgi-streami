13830115a0ba70568f9c6d619a2c092cfa4c0e78
Naegato <maxime.wiatr@gmail.com>
2024-12-05 22:26:36 +0100

    feat/dynamic-page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
+    public function __construct(private readonly MovieRepository $movieRepository)
+    {
+    }
+
     #[Route('/', name: 'default')]
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        $movies = $this->movieRepository->findAll();
+
+    return $this->render('index.html.twig', [
+            'movies' => $movies
+        ]);
     }
 }
